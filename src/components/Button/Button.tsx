f08e4f9b8ef030fa080063ea4eef6b063988c134
Diana Selyuk <selukdiana@gmail.com>
2022-08-23 19:31:17 +0300

    docs: renamed components

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+import './Button.css';
+import { IButtonProps } from '../../types/props';
+
+export const Button = ({
+  onClick,
+  type,
+  children,
+  size = 's',
+}: IButtonProps) => {
+  const btnClass = classNames({
+    btn: true,
+    'btn--secondary': type === 'secondary',
+    'btn--primary': type === 'primary',
+    'btn--small': size === 's',
+    'btn--medium': size === 'm',
+  });
+
+  return (
+    <button className={btnClass} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
---
+++ src/ui-kit/Search/Search.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { ISearchProps } from '../../types/props';
+import './Search.css';
+
+export const Search = ({ handleClick }: ISearchProps) => {
+  return (
+    <div className="search">
+      <AiOutlineSearch className="search__icon" />
+      <input
+        placeholder="Search..."
+        className="search__input"
+        onKeyPress={handleClick}
+      />
+    </div>
+  );
+};
